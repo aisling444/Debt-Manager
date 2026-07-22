75d1ace8b33add7cbecc773bbb6d1d13cc2a4388
aisling444 <aislingwalshnibreathnach@gmail.com>
2026-07-22 18:56:24 +0100

    Imported ExpenseLogger

--- App.jsx
+++ App.jsx
@@ -1,9 +1,12 @@
 import GroupSetup from './components/GroupSetup'
+import ExpenseLogger from './components/ExpenseLogger'
+
 function App() {
   return (
     <div>
       <h1>Bill Splitter</h1>
       <GroupSetup />
+      <ExpenseLogger />
     </div>
   )
 }
