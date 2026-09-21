b8284ee5db2addf5be7d0096c16df59b20e2ebda
aisling444 <aislingwalshnibreathnach@gmail.com>
2026-09-21 18:36:26 +0100

    Added zustand persit data, so data survives a refresh

--- store/useStore.js
+++ store/useStore.js
@@ -1,15 +1,24 @@
 import { create } from 'zustand'
+import { persist, createJSONStorage } from 'zustand/middleware'
 
-const useStore = create((set) => ({
-  people: [],
-  expenses: [],
-  settlements: [],
+const useStore = create(
+  persist(
+    (set) => ({
+      people: [],
+      expenses: [],
+      settlements: [],
 
-  addPerson: (name) => set((state) => ({ people: [...state.people, {id: crypto.randomUUID(), name }], })),
+      addPerson: (name) => set((state) => ({ people: [...state.people, { id: crypto.randomUUID(), name }] })),
 
-  addExpense: ({ paidBy, amount, splitBetween, description }) => set((state) => ({ expenses: [...state.expenses, { id: crypto.randomUUID(), paidBy, amount, splitBetween, description, }, ], })),
+      addExpense: ({ paidBy, amount, splitBetween, description }) => set((state) => ({ expenses: [...state.expenses, { id: crypto.randomUUID(), paidBy, amount, splitBetween, description }] })),
 
-  settleDebt: ({ from, to, amount }) => set((state) => ({ settlements: [...state.settlements, { id: crypto.randomUUID(), from, to, amount, }, ], })),
-}))
+      settleDebt: ({ from, to, amount }) => set((state) => ({ settlements: [...state.settlements, { id: crypto.randomUUID(), from, to, amount }] })),
+    }),
+    {
+      name: 'bill-splitter-storage',
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+)
 
 export default useStore
